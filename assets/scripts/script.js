a7fb80cdf6eefc97d94f44ea3eb8f5fe6a4803fa
Informatica <informatica@alumno.local>
2024-01-23 17:32:56 -0600

    Se modificó el js para aparecer y desaparecer los videojuegos

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,9 +2,20 @@ function cambiarFondoResident(){
     let mybody=document.getElementById('body');
     mybody.style.backgroundImage = "url(https://mx.c3kienthuyhp.edu.vn/wp-content/uploads/2023/03/1680144201_Como-derrotar-a-Osmund-Saddler-en-Resident-Evil-4-Remake.jpg)";
 }
-function desaparecerRE(){
-    const desaparecerOw = document.getElementById('overwatch');
-    const desaparecerER = document.getElementById('eldenring');
+var desaparecerOw = document.getElementById('overwatch');
+var desaparecerER = document.getElementById('eldenring');
+
+async function desaparecerRE(){ 
+    var x = temporizadorRE(40)
     desaparecerOw.style.display = 'none';
     desaparecerER.style.display = 'none';
+    console.log(x);
+}
+function temporizadorRE(){
+    return new Promise((resolve) => {
+        setTimeout(() =>{
+            desaparecerOw.style.display = 'block';
+            desaparecerER.style.display = 'block';
+        }, 6000)
+    })
 }
