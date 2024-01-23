531f1bae2c64d467dc4b7169ec94335c3c202337
Informatica <informatica@alumno.local>
2024-01-23 16:18:02 -0600

    Creación de Pasatiempos, CSS de pasatiempos y JS de Pasatiempos

---
+++ assets/scripts/script.js
@@ -0,0 +1,12 @@
+function cambiarFondoResident(){
+    let mybody=document.getElementById('body');
+    mybody.style.backgroundImage = "url(https://mx.c3kienthuyhp.edu.vn/wp-content/uploads/2023/03/1680144201_Como-derrotar-a-Osmund-Saddler-en-Resident-Evil-4-Remake.jpg)";
+}
+function desaparecer(){
+    const desaparecer = document.getElementById('overwatch');
+    desaparecer.style.display = 'none';
+}
+function aparecer(){
+    const aparecer = document.getElementById('overwacth');
+    desaparecer.style.display;
+}
