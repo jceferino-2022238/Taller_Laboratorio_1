46a4042125b0e93a867c4c666c7041a3c47fc3bb
Informatica <informatica@alumno.local>
2024-01-23 17:22:28 -0600

    Se agregaron id a los details y se expandió el espacio de los videojuegos

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,11 +2,9 @@ function cambiarFondoResident(){
     let mybody=document.getElementById('body');
     mybody.style.backgroundImage = "url(https://mx.c3kienthuyhp.edu.vn/wp-content/uploads/2023/03/1680144201_Como-derrotar-a-Osmund-Saddler-en-Resident-Evil-4-Remake.jpg)";
 }
-function desaparecer(){
-    const desaparecer = document.getElementById('overwatch');
-    desaparecer.style.display = 'none';
-}
-function aparecer(){
-    const aparecer = document.getElementById('overwacth');
-    desaparecer.style.display;
+function desaparecerRE(){
+    const desaparecerOw = document.getElementById('overwatch');
+    const desaparecerER = document.getElementById('eldenring');
+    desaparecerOw.style.display = 'none';
+    desaparecerER.style.display = 'none';
 }
