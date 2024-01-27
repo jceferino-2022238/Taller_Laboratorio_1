77197aa9c98d354670819cd75b61122fa3c4eecb
jceferino-2022238 <jceferino-2022238@kinal.edu.gt>
2024-01-26 23:34:24 -0600

    Imagenes de seleccion creadas

--- assets/scripts/scriptK.js
+++ assets/scripts/scriptK.js
@@ -13,3 +13,35 @@ function mostrar4(){
 function mostrar5(){
     document.getElementById('opciones5').classList.toggle('show');
 }
+const slides = document.querySelectorAll('.slides img');
+let slideIndex = 0;
+let intervalId = null;
+
+document.addEventListener('DOMContentLoaded', inicioSlider);
+
+function inicioSlider(){
+    if(slides.length > 0){
+        slides[slideIndex].classList.add('displaySlide');
+        intervalId = setInterval(slideSig, 5000);
+    } 
+}
+function mostrarSlide(index){
+    if(index >= slides.length){
+        slideIndex = 0
+    }else if(index  < 0){
+        slideIndex = slides.length -1;
+    }
+    slides.forEach(slide => {
+        slide.classList.remove('displaySlide');
+    });
+    slides[slideIndex].classList.add('displaySlide');
+}
+function slidePre(){
+    clearInterval(intervalId);
+    slideIndex--;
+    mostrarSlide(slideIndex);
+}
+function slideSig(){
+    slideIndex++;
+    mostrarSlide(slideIndex);
+}
