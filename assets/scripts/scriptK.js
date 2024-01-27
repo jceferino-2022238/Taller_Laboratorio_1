90ac1bfa6c1bd603a463dbfe6722cee943ebd46d
jceferino-2022238 <jceferino-2022238@kinal.edu.gt>
2024-01-26 22:32:50 -0600

    Se creo el header de la página de kinal

---
+++ assets/scripts/scriptK.js
@@ -0,0 +1,15 @@
+function mostrar1(){
+    document.getElementById('opciones1').classList.toggle('show');
+}
+function mostrar2(){
+    document.getElementById('opciones2').classList.toggle('show');
+}
+function mostrar3(){
+    document.getElementById('opciones3').classList.toggle('show');
+}
+function mostrar4(){
+    document.getElementById('opciones4').classList.toggle('show');
+}
+function mostrar5(){
+    document.getElementById('opciones5').classList.toggle('show');
+}
